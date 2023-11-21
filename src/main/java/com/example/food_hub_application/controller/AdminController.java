6c5a21b062c5169786052acfbe5f91c2e95ebbf5
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2023-11-21 10:28:12 +0600

    add product delete and update in project for admin panel

--- src/main/java/com/example/food_hub_application/controller/AdminController.java
+++ src/main/java/com/example/food_hub_application/controller/AdminController.java
@@ -101,6 +101,29 @@ public class AdminController {
 
     }
 
+    @GetMapping("/admin/product/delete/{id}")
+    public String deleteProduct(@PathVariable long id) {
+        productService.removeProductById(id);
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/admin/product/update/{id}")
+    public String updateProduct(@PathVariable long id, Model model){
+        Product product = productService.getProductById(id).get();
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(product.getId());
+        productDTO.setName(product.getName());
+        productDTO.setCategoryId(product.getCategory().getId());
+        productDTO.setPrice(product.getPrice());
+        productDTO.setWeight(product.getWeight());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setImageName(product.getImageName());
+
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("productDTO", productDTO);
+
+        return "productsAdd";
+    }
 
 
 
